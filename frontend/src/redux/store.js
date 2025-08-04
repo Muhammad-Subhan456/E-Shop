37b7583b91ce3b52825b951ceede213e2abfba77
Muhammad-Subhan456 <sbhnamir456@gmail.com>
2025-08-04 12:32:58 +0500

    create product api created

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,11 +1,14 @@
 import {configureStore} from "@reduxjs/toolkit"
 import {userReducer} from "./reducers/user"
 import {sellerReducer} from "./reducers/seller"
+import { productReducer } from "./reducers/product";
+
 
 const store = configureStore({
     reducer:{
         user: userReducer,
         seller: sellerReducer,
+        products: productReducer,
     }
 })
 
