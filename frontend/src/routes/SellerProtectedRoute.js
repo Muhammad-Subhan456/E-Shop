6c7ce517ec56c2c612e1272042250a4ab5c375fe
Muhammad-Subhan456 <sbhnamir456@gmail.com>
2025-08-04 10:28:19 +0500

    created shop home page

--- frontend/src/routes/SellerProtectedRoute.js
+++ frontend/src/routes/SellerProtectedRoute.js
@@ -1,17 +1,21 @@
 import { useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-
+import Loader from "../components/Layout/Loader"
 const SellerProtectedRoute = ({ children }) => {
     const {isLoading, isSeller,seller } = useSelector((state)=>state.seller) 
   const navigate = useNavigate();
 
- if(isLoading === false){
-   if (!isSeller) {
-    navigate(`/`, { replace: true });
-    return;
+ if(isLoading === true){
+  //return <Loader/>
+}
+  else{
+
+    if (!isSeller) {
+      navigate(`/`, { replace: true });
+      return;
+    }
+    return children;
   }
-  return children;
- }
 };
 
 export default SellerProtectedRoute;
