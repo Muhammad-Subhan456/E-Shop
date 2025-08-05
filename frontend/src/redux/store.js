4d3a14ebdd918f7b8b7b50af94c06d35f4f4c7b3
Muhammad-Subhan456 <sbhnamir456@gmail.com>
2025-08-06 00:08:52 +0500

    created actions and reducers of createEvent

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,6 +2,7 @@ import {configureStore} from "@reduxjs/toolkit"
 import {userReducer} from "./reducers/user"
 import {sellerReducer} from "./reducers/seller"
 import { productReducer } from "./reducers/product";
+import { eventReducer } from "./reducers/event";
 
 
 const store = configureStore({
@@ -9,6 +10,7 @@ const store = configureStore({
         user: userReducer,
         seller: sellerReducer,
         products: productReducer,
+        events: eventReducer,
     }
 })
 
